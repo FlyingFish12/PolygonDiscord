238161685df24f92f2602b6b87e5c4c679228ce1
Fefe <F33f11f00fum@gmail.com>
2022-04-14 19:00:55 +0100

    removed counting stuff

--- Schemas/countDB.js
+++
@@ -1,8 +0,0 @@
-const { model, Schema } = require("mongoose");
-
-module.exports = model("count", new Schema({
-    GuildID: String,
-    ChannelID: String,
-    Count: Number,
-    CountBy: String
-}));
